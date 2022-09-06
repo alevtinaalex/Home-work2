43ec6027e8e4a2ad0e421f38baa5d9d50c4f4566
Alevtina <_@email.comgit>
2022-09-06 21:10:59 +0300

    Add vivod 3 cifra chisla

---
+++ viv_3cifr_chisla/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число и нажмите Enter");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99)
+ {
+   int a = num % 10;
+   int index = 2;
+   int chis = num.ToString()[index] - '0';
+    if (chis >= 0)
+     {
+     Console.WriteLine("Третья цифра в числе");
+     Console.WriteLine(chis);
+     }
+  }
+else
+   {
+      Console.WriteLine("Третьей цифры в этом числе нет");
+   } 
