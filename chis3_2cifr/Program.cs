af51237fb4e4521271f598c0cf1a481bc5744f82
Alevtina <_@email.comgit>
2022-09-06 13:43:15 +0300

    Vivod 2 cifr 3znachnogo chisla

---
+++ chis3_2cifr/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число и нажмите Enter");
+int num = Convert.ToInt32(Console.ReadLine());
+int b = ((num /10) % 10);
+Console.Write("Вторая цифра числа ");
+Console.Write(b);
