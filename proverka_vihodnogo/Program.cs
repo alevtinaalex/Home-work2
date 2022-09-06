6b8e72cdb3342c4f5d7ee0fc86a37cd6647edaf3
Alevtina <_@email.comgit>
2022-09-06 19:24:26 +0300

    Add check satturday and sunday

---
+++ proverka_vihodnogo/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите цифру дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch(day)
+  {
+    case 1:
+         Console.WriteLine("Это рабочий день"); break;
+    case 2:
+         Console.WriteLine("Это рабочий день"); break;
+    case 3:
+         Console.WriteLine("Это рабочий день"); break;
+    case 4:
+         Console.WriteLine("Это рабочий день"); break;
+    case 5:
+         Console.WriteLine("Это рабочий день"); break;
+    case 6:
+         Console.WriteLine("Это выходной день"); break;
+    case 7:
+         Console.WriteLine("Это выходной день"); break;
+   }
